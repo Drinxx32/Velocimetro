a24ce623260a870e9d3bc8e2daa1aed5141ec8f2
Drinxx32 <henriquesadvr@gmail.com>
2024-10-14 15:31:50 -0300

    01

---
+++ script.js
@@ -0,0 +1,8 @@
+var vel = 60
+console.log (`a velocidade do seu carro é ${vel}Km/h`)
+if (vel>60){
+    console.log('você ultrapassou a velocidade e foi multado')
+}
+else{
+    console.log('você é habilitado') 
+}
